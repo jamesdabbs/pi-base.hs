faa09c924fd6a453e0ace060859a91496bec02e1
James Dabbs <jamesdabbs@gmail.com>
2014-05-23 19:44:13 -0400

    Slight search handler cleanup
    This will need to become an Html Handler, but only after we can parse
    formulae from a string description.

--- Handler/Search.hs
+++ Handler/Search.hs
@@ -2,18 +2,12 @@ module Handler.Search where
 
 import Import
 
-import Data.Aeson
-import qualified Data.ByteString.Lazy as LBS
-import Data.Text.Encoding (encodeUtf8)
-
 import Logic.Types (MatchType(..))
 import Logic (matches)
+import Util (decodeText)
 
 jsonError :: Text -> Handler Value
 jsonError str = returnJson $ object [ "error" .= str ]
-
-decodeText :: FromJSON a => Text -> Maybe a
-decodeText = decode . LBS.fromStrict . encodeUtf8
 
 getSearchR :: Handler Value
 getSearchR = do
