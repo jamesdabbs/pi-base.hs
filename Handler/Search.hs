73d0cdecf7e0d8fa47771a5835d361d8e8ce0a06
James Dabbs <jamesdabbs@gmail.com>
2014-06-07 21:58:28 -0400

    Rudimentary search-by-text #6

--- Handler/Search.hs
+++ Handler/Search.hs
@@ -4,6 +4,7 @@ import Import
 import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Text as T
+import Database.Persist.Sql (rawSql)
 
 import Handler.Helpers
 import Handler.Partials (linkedFormula)
@@ -25,14 +26,18 @@ formulaLookup f = do
 searchShow :: Maybe Text -> Widget
 searchShow q = $(widgetFile "search/show")
 
-searchByText :: Text -> Handler Html
-searchByText = undefined
+searchByText :: Text -> Text -> Handler Html
+searchByText qt text = do
+  let q = "SELECT ?? FROM spaces WHERE spaces.name ILIKE ?"
+  spaces <- runDB $ rawSql q [PersistText $ "%" <> text <> "%"]
+  render "Search" $(widgetFile "search/text_results")
 
 matchTypeDisplay :: MatchType -> Text
 matchTypeDisplay Yes = "∋"
 matchTypeDisplay No = "∌"
 matchTypeDisplay Unknown = "?"
 
+-- TODO: search across spaces, properties, descriptions, aliases ... probably want ES for that
 searchByFormula :: Text -> MatchType -> Text -> Handler Html
 searchByFormula qt _type text = do
   let mf = decodeText text
@@ -54,7 +59,7 @@ getSearchR = do
     Nothing -> render "Search" (searchShow q)
     Just qt -> do
       case T.uncons qt of
-        Just (':', t) -> searchByText t
+        Just (':', t) -> searchByText qt t
         Just ('?', t) -> searchByFormula qt Unknown t
         Just ('!', t) -> searchByFormula qt No t
         Just _ -> searchByFormula qt Yes qt
